25627bd14e3d9e4b1601d474b4087c17c42c4e6a
Coleman Buffa <coleman.buffa@gmail.com>
2022-04-16 19:45:10 -0700

    Fixes #2 Desgin v2 API with Swagger

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const routes = require('./controllers');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDoc = require('./swagger.json');
+const swaggerV1 = require('./swagger-v1.json');
+const swaggerV2 = require('./swagger-v2.json');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,10 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 app.use(
-  '/docs',
-  swaggerUi.serveFiles(swaggerDoc),
-  swaggerUi.setup(swaggerDoc)
+  '/docs/v1',
+  swaggerUi.serveFiles(swaggerV1),
+  swaggerUi.setup(swaggerV1)
 );
+app.use(
+  '/docs/v2',
+  swaggerUi.serveFiles(swaggerV2),
+  swaggerUi.setup(swaggerV2)
+);
+
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
